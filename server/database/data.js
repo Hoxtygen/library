95e98d3a616ccb5f064fc9d8b64038c6d7b33a92
Idowu Wasiu <hoxtygen@live.com>
2018-11-07 00:07:22 +0100

    Initial commit

---
+++ server/database/data.js
@@ -0,0 +1,46 @@
+const Data = [
+  {
+    id: 1,
+    bookTitle: 'Trunk Music',
+    Author: 'Michael Connelly',
+    pubYear: 1997,
+    publisher: 'Orion Books',
+  },
+  {
+    id: 2,
+    bookTitle: 'Roadside Crosses',
+    Author: 'Jeffery Deaver',
+    pubYear: 2009,
+    publisher: 'Pocket Books',
+  },
+  {
+    id: 3,
+    bookTitle: 'The Broken Window',
+    Author: 'Jeffery Deaver',
+    pubYear: 2008,
+    publisher: 'Pocket Books',
+  },
+  {
+    id: 4,
+    bookTitle: 'The Sabbathday River',
+    Author: 'Jean Hanff Korelitz',
+    pubYear: 1990,
+    publisher: 'Jove Books',
+  },
+  {
+    id: 5,
+    bookTitle: 'The Poet',
+    Author: 'Michael Connelly',
+    pubYear: 1996,
+    publisher: 'Warner Books',
+  },
+  {
+    id: 6,
+    bookTitle: 'The Twelfth Card',
+    Author: 'Jeffery Deaver',
+    pubYear: 2005,
+    publisher: 'Pocket Books',
+  },
+];
+
+export default Data;
