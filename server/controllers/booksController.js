9180e4c76d03e9bb944ea6b067ecd4cdccd94ffa
Idowu Wasiu <hoxtygen@live.com>
2018-11-08 11:43:26 +0100

    ft(feature):add POST request endpoint
    ensure new book can be added to the database
    ensure POST request can be done
    
    [Delivers ft-feature-endpoint-post]

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -9,6 +9,30 @@ class BookController {
       Books,
     });
   }
+
+  static addNew(req, res) {
+    const {
+      title, Author, pubYear, publisher,
+    } = req.body;
+    const newBook = {
+      id: Books.length + 1,
+      title,
+      Author,
+      pubYear,
+      publisher,
+    };
+    if (!title || !Author || !pubYear || !publisher) {
+      res.status(400).json({
+        message: 'Missing fields present',
+      });
+    }
+    //  console.log(newBook);
+    Books.push(newBook);
+    return res.status(201).json({
+      message: 'book added',
+      newBook,
+    });
+  }
 }
 
 export default BookController;
--- server/tests/booksTest.js
+++ server/tests/booksTest.js
@@ -15,3 +15,29 @@ describe('GET all books from the database', () => {
       });
   });
 });
+
+describe('POST a new book to the database', () => {
+  it('should add a new book to the database', (done) => {
+    const Item = {
+      id: 8,
+      title: 'The yellow leopard',
+      Author: 'shaolin Dragon',
+      pubYear: 2004,
+      publisher: 'dragon books',
+    };
+    chai.request(app)
+      .post('/api/data/books')
+      .send(Item)
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.should.be.a('object');
+        res.body.should.have.property('message').eql('book added');
+        res.body.newBook.should.have.a.property('id');
+        res.body.newBook.should.have.a.property('title');
+        res.body.newBook.should.have.property('Author');
+        res.body.newBook.should.have.property('pubYear');
+        res.body.newBook.should.have.property('publisher');
+        done();
+      });
+  });
+});
