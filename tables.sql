e518f6b800f755b25ed2645ddf351a0d3bd85d3c
Idowu Wasiu <hoxtygen@live.com>
2018-11-10 18:01:24 +0100

    ch(styling):style header section
    ensure header has styling applied

--- tables.sql
+++ tables.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS books (
+\c library
+CREATE TABLE IF NOT EXISTS leaders (
     id SERIAL PRIMARY KEY NOT NULL,
     title VARCHAR (400) NOT NULL,
     author VARCHAR (100) NOT NULL,
