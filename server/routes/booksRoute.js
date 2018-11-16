54c71eb245e4922900695f2bb7b7e9cbc912e5df
Idowu Wasiu <hoxtygen@live.com>
2018-11-16 16:08:37 +0100

    add title, author and category query parameter

--- server/routes/booksRoute.js
+++ server/routes/booksRoute.js
@@ -7,5 +7,7 @@ booksRouter.get('/books/:id', BookController.getOne);
 booksRouter.post('/books', BookController.addNew);
 booksRouter.delete('/books/:id', BookController.deleteBook);
 booksRouter.put('/books/:id', BookController.update);
-
+booksRouter.get('/books/title/:title', BookController.getTitle);
+booksRouter.get('/books/author/:author', BookController.getAuthor);
+booksRouter.get('/books/category/:category', BookController.getCategory);
 export default booksRouter;
