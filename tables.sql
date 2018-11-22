7949389e5212548a9295329ca943ea08e11d548f
Idowu Wasiu <hoxtygen@live.com>
2018-11-22 10:36:29 +0100

    connect to database using personal account

--- tables.sql
+++ tables.sql
@@ -1,3 +1,2 @@
-\c library
+\c library hoxtygen
 
-INSERT INTO book_library.books (title, pubyear, publisher, author_id, category_id, image_url) VALUES ('The SabbathDay River', 1999, 'Jove Books Ltd', 9, 1, 'http://i38.photobucket.com/albums/e143/hoxtygen/sabbathDay_zps3zsxhz3z.jpg');
