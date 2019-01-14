cc164ce9eebf0d46b68b2fa3e1a75ddd658afd69
Idowu Wasiu <hoxtygen@live.com>
2019-01-14 23:31:09 +0100

    ft(feature): add styling
    ensure UI is appropriately styled
    ensure page elements are well aligned
    
    [Delivers ch-chore-frontend-UI-styling]

---
+++ UI/assets/js/formData.js
@@ -0,0 +1,33 @@
+function getAllCategories() {
+  fetch('http://localhost:5001/api/v1/categories')
+    .then(res => res.json())
+    .then((parsedCategories) => {
+      const myData = parsedCategories;
+      const select = document.getElementById('categoryId');
+      myData.data.map((data) => {
+        const option = document.createElement('OPTION');
+        option.value = data.category_id;
+        option.text = data.category_name;
+        select.insertBefore(option, select.lastChild);
+      });
+    });
+}
+getAllCategories();
+
+function getAllAuthors() {
+  fetch('http://localhost:5001/api/v1/authors')
+    .then(res => res.json())
+    .then((parsedAuthors) => {
+      //  console.log(parsedAuthors);
+      const myData = parsedAuthors;
+      const select = document.getElementById('authorId');
+      myData.data.map((data) => {
+        const option = document.createElement('OPTION');
+        option.value = data.author_id;
+        option.text = data.author_name;
+        select.insertBefore(option, select.lastChild);
+      });
+    });
+}
+
+getAllAuthors();
